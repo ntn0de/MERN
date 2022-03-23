471d16aca503e05a0fc68d86705a1f68c15827ef
ntn0de <ntn0de@gmail.com>
2022-03-23 12:13:39 +0545

    REST API for Goals

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -18,21 +18,38 @@ const setGoals = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Please add a text field");
   }
-  res.status(200).json({ message: "set goals" });
+  const goal = await Goal.create({
+    text: req.body.text,
+  });
+  res.status(200).json(goal);
 });
 
 // @desc Update Goals
 // @route PUT /api/goals
 // access Private
 const updateGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `update goal ${req.params.id}` });
+  const goal = await Goal.findById(req.params.id);
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal doesn't exist");
+  }
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.status(200).json(updatedGoal);
 });
 
 // @desc Delete Goals
 // @route DELETE /api/goals
 // access Private
 const deleteGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `delete goal ${req.params.id}` });
+  const goal = await Goal.findById(req.params.id);
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal doesn't exist");
+  }
+  await Goal.findByIdAndRemove(req.params.id);
+  res.status(200).json({ message: `deleted goal with id ${req.params.id}` });
 });
 
 module.exports = {
--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,11 @@ const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
 
-connectDB()
+connectDB();
 
 const app = express();
 
-app.use(express.json);
+// app.use(express.json);
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/goals", require("./routes/goalRoutes"));
